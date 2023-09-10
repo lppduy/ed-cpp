fbc234baab78f2db1a99331f43313fc7e7d07491
lppduy <lephamphuongduy@gmail.com>
2023-09-10 12:49:30 +0700

    v10

--- basic_level_1/hello.cpp
+++ basic_level_1/hello.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <vector>
+
 
 int main()
 {
-  std::cout << "Hell" << std::endl;
-  std::cout << "LPPD" << std::endl;
+  
+  const int seconds_per_hour = 3600;
+  const double max_allowed_temperature = 76.8;
+
+  int user_age = 34;
+  double temperature = 20.6;
+  bool is_alive = false;
+  std::string user_name = "James";
+
+  std::cout << seconds_per_hour << std::endl;
+  std::cout << max_allowed_temperature << std::endl;
+  
   return 0;
 } 
+
